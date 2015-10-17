d1e1719695ed96b91fa93370f4381308c2c5b87d
Alex Gamas <alexgamas@gmail.com.br>
2015-10-17 18:29:47 -0300

    Modificações Treinamento 4 - Adicao de bootstrap;

--- treinamento_4/src/main/java/br/com/gamas/treinamento/security/SecurityConfiguration.java
+++ treinamento_4/src/main/java/br/com/gamas/treinamento/security/SecurityConfiguration.java
@@ -12,36 +12,24 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebMvcSecurity
-// @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-// @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
     private UserDetailsService userDetailsService;
 	
+	private String[] AREA_PUBLICA = {"/js/**", "/css/**", "/images/**", "/fonts/**", "/", "/home", "/formulario/inscricao", "/formulario/visualizar/*", "/formulario/salvar"};
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		//super.configure(http);
 		// @formatter:off
-		HttpSecurity httpSecurity = http.authorizeRequests().antMatchers("/images/**", "/", "/home", "/formulario/inscricao").permitAll().anyRequest().fullyAuthenticated().and();
+		HttpSecurity httpSecurity = http.authorizeRequests().antMatchers(AREA_PUBLICA).permitAll().anyRequest().fullyAuthenticated().and();
 		httpSecurity = httpSecurity.formLogin().loginPage("/login").failureUrl("/login?error").permitAll().and();
 		httpSecurity = httpSecurity.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/").and();
-		//httpSecurity = httpSecurity.exceptionHandling().accessDeniedPage("/access?error").and();
 		// @formatter:on
-		
-		//.hasAuthority("ADMIN")
 	}
-	
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-//		InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> inMemoryAuth = auth.inMemoryAuthentication();
-//        inMemoryAuth.withUser("admin").password("admin").roles("ADMIN", "USER");
-//        inMemoryAuth.withUser("user").password("user").roles("USER");
-//        inMemoryAuth.withUser("alex").password("gamas").roles("USER");
-		
 		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-	
 	}
 }
