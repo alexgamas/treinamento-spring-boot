b87af4532fa7c3b97441675fffb91ca483044e3e
Alex Gamas <alex@gamas.com.br>
2015-10-17 14:06:00 -0300

    Remoção de warning

--- treinamento_1/src/main/java/br/com/gamas/treinamento/controller/HomeController.java
+++ treinamento_1/src/main/java/br/com/gamas/treinamento/controller/HomeController.java
@@ -2,15 +2,11 @@ package br.com.gamas.treinamento.controller;
 
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
 	@RequestMapping("/")
 	public String root(Map<String, Object> model) {
