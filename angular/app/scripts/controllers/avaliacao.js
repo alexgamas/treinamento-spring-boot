609ae534820e23d8f2c55a289320a780dca45589
Alex Gamas <alexgamas@gmail.com>
2016-10-13 18:02:16 -0300

    avaliacao

--- angular/app/scripts/controllers/avaliacao.js
+++ angular/app/scripts/controllers/avaliacao.js
@@ -7,6 +7,13 @@
  * # AvaliacaoCtrl
  * Controller of the treinamentoApp
  */
-angular.module('treinamentoApp').controller('AvaliacaoCtrl', function ($scope) {
+angular.module('treinamentoApp').controller('AvaliacaoCtrl', function ($scope, $http) {
+
+
+	$http.get('http://localhost:3000/inscricoes').then(function (response) {
+		$scope.inscricoes = response.data;
+	});
+
+
 
 });
--- treinamento_6/src/main/java/br/com/gamas/treinamento/repository/InscricaoRepositoryImpl.java
+++
@@ -1,5 +0,0 @@
-package br.com.gamas.treinamento.repository;
-
-public class InscricaoRepositoryImpl {
-
-}
